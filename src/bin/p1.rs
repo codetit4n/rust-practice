56ff1b6d0bd031be2a55af259cba5d5e3b6988e9
Lokesh Kumar Nalot <lokeshkumar@M1-Mac.local>
2022-01-18 02:08:59 +0530

    update 1

--- src/bin/p1.rs
+++ src/bin/p1.rs
@@ -18,46 +18,88 @@
 // * Create your program starting at level 1. Once finished, advance to the
 //   next level.
 
+use std::collections::HashMap;
 use std::io;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Bill {
     name: String,
     amount: f64,
 }
 
 struct Bills {
-    inner: Vec<Bill>, //inner means we are taking the inner values of bills
+    inner: HashMap<String, Bill>, //inner means we are taking the inner values of bills
 }
 
 impl Bills {
     fn new() -> Self {
-        Self { inner: vec![] }
+        Self {
+            inner: HashMap::new(),
+        }
     }
 
     fn add(&mut self, bill: Bill) {
-        self.inner.push(bill);
+        self.inner.insert(bill.name.clone(), bill);
+        //reason to clone is because the key
+        //needs to be a owned string and bill name also need to be a owned string
     }
 
-    fn get_all(&self) -> &Vec<Bill> {
-        return &self.inner;
+    fn get_all(&self) -> Vec<Bill> {
+        //here we will build a vector from hashmap
+        let mut bills = vec![];
+        for bill in self.inner.values() {
+            //here we are only interested in values
+            bills.push(bill.clone());
+        }
+        //note when iterating through hashmaps bill in for loop is always borrowed
+        //and we don't want to push in a borrowed item so we clone
+        bills //note here we are returning a owned vector not borrowed so after this
+              //function is done executing it will transfer the ownership of this
+              //vector to wherever it is called
+    }
+
+    fn remove(&mut self, name: &str) -> bool {
+        self.inner.remove(name).is_some() //is_some() returns true if option is some
+                                          //value
+    }
+    fn update(&mut self, name: &str, amount: f64) -> bool {
+        match self.inner.get_mut(name) {
+            //get_mut returns an optional mutable value
+            //it allows us to mutate the value
+            Some(bill) => {
+                bill.amount = amount;
+                true
+            }
+            None => false,
+        }
     }
 }
-fn get_input() -> String {
+fn get_input() -> Option<String> {
     let mut buffer = String::new();
     while io::stdin().read_line(&mut buffer).is_err() {
         println!("Please enter your data again");
     }
-    buffer.trim().to_owned()
+    let input = buffer.trim().to_owned();
+    if &input == "" {
+        None
+    } else {
+        Some(input)
+    }
 }
 
-fn get_bill_amount() -> f64 {
+fn get_bill_amount() -> Option<f64> {
     println!("Enter amount:");
     loop {
-        let input: String = get_input(); //getting input
+        let input = match get_input() {
+            Some(input) => input,
+            None => return None,
+        };
+        if &input == "" {
+            return None;
+        }
         let parsed_input: Result<f64, _> = input.parse(); //parsing input
         match parsed_input {
-            Ok(amt) => return amt,
+            Ok(amt) => return Some(amt),
             Err(_) => println!("Please enter a number: "),
         }
     }
@@ -66,13 +108,59 @@ fn get_bill_amount() -> f64 {
 fn add_bill_menu(bills: &mut Bills) {
     //get the bill name
     println!("Enter bill name:");
-    let name = get_input();
+    let name = match get_input() {
+        Some(input) => input,
+        None => return,
+    };
     //get the bill amount
-    let amount = get_bill_amount();
-    let bill = Bill { name, amount }; //since names and variables of struct are same we
-                                      //don't need to specify. Compiler will figure it out
+    let amount = match get_bill_amount() {
+        Some(amount) => amount,
+        None => return,
+    };
+    let bill = Bill { name, amount };
+    //since names and variables of struct are same we
+    //don't need to specify. Compiler will figure it out
     bills.add(bill);
     println!("Bill added");
+}
+
+fn remove_bill_menu(bills: &mut Bills) {
+    //first we need to view the bills we are deleting
+    for bill in bills.get_all() {
+        println!("{:?}", bill);
+    }
+    println!("Enter bill name to remove:");
+    //input
+    let input = match get_input() {
+        Some(input) => input,
+        None => return,
+    };
+    if bills.remove(&input) {
+        println!("removed");
+    } else {
+        println!("bill not found!");
+    }
+}
+
+fn update_bill_menu(bills: &mut Bills) {
+    //first we need to view the bills we are deleting
+    for bill in bills.get_all() {
+        println!("{:?}", bill);
+    }
+    println!("Enter bill name to update:");
+    let name = match get_input() {
+        Some(name) => name,
+        None => return,
+    };
+    let amount = match get_bill_amount() {
+        Some(amount) => amount,
+        None => return,
+    };
+    if bills.update(&name, amount) {
+        println!("updated")
+    } else {
+        println!("bill not found!")
+    }
 }
 
 fn view_bill_menu(bills: &Bills) {
@@ -88,6 +176,9 @@ fn main_menu() {
         println!("=== Manage Bills ===");
         println!("1. Add bills");
         println!("2. View bills");
+        println!("3. Remove bill");
+        println!("4. Update bill");
+        println!("Enter nothing to exit");
         println!("");
         println!("Enter selection:");
     }
@@ -96,10 +187,15 @@ fn main_menu() {
 
     loop {
         show();
-        let input = get_input();
+        let input = match get_input() {
+            Some(input) => input,
+            None => return,
+        };
         match input.as_str() {
             "1" => add_bill_menu(&mut bills), //"1" automatically makes borrowed strings
             "2" => view_bill_menu(&bills),    //here its not going to modify bill so we borrow
+            "3" => remove_bill_menu(&mut bills),
+            "4" => update_bill_menu(&mut bills),
             _ => break,
         }
     }
