87bb2d17a7fcee158598657bfea5fad787f0c02d
Lokesh Kumar Nalot <lokeshkumar@M1-Mac.local>
2022-01-26 01:45:00 +0530

    update 5

--- src/bin/a26.rs
+++ src/bin/a26.rs
@@ -7,5 +7,12 @@
 // * Use the `chrono` crate to work with time
 // * (OPTIONAL) Read the documentation section `Formatting and Parsing`
 //   for examples on how to create custom time formats
+use chrono::{DateTime, Local};
 
-fn main() {}
+fn main() {
+    let dt: DateTime<Local> = Local::now();
+    println!("{:?}", dt);
+
+    //printing in a formatted way: see documentation
+    println!("{:?}", dt.format("%a %b %e %T %Y").to_string());
+}
