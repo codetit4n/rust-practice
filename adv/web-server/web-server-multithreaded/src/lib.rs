0769e6c9b571ea13041f5cd1e3ba3c6f60ee86a1
codeTIT4N <lokeshkumar636@yahoo.com>
2023-07-24 21:34:19 +0530

    updated - multithreaded web server

--- adv/web-server/web-server-multithreaded/src/lib.rs
+++ adv/web-server/web-server-multithreaded/src/lib.rs
@@ -1,4 +1,28 @@
-pub struct ThreadPool;
+use std::{
+    sync::{mpsc, Arc, Mutex},
+    thread,
+};
+
+struct Job;
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(|| {
+            receiver;
+        });
+        Worker { id, thread }
+    }
+}
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
+}
 
 impl ThreadPool {
     /// Create a new ThreadPool.
@@ -8,15 +32,26 @@ impl ThreadPool {
     /// # Panics
     ///
     /// The `new` function will panic if the size is zero.
-    pub fn new(size: usize) -> Self {
+    pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
-        Self
+
+        let (sender, receiver) = mpsc::channel();
+
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
+        }
+
+        ThreadPool { workers, sender }
     }
 
     pub fn execute<F>(&self, _f: F)
     where
         F: FnOnce() + Send + 'static,
     {
+        // todo
     }
 }
-// TBC
