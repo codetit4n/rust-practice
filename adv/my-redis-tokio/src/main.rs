7886b081607a7e616a9c1635939cf5b09e224966
codeTIT4N <lokeshkumar636@yahoo.com>
2023-08-30 21:45:46 +0530

    typo

--- adv/my-redis-tokio/src/main.rs
+++ adv/my-redis-tokio/src/main.rs
@@ -5,7 +5,7 @@ async fn main() -> Result<()> {
     // Open a connection to the mini-redis address.
     let mut client = client::connect("127.0.0.1:6379").await?;
 
-    // Set the key "hello" with value "world"
+    // Set the key "foo" with value "bar"
     client.set("foo", "bar".into()).await?;
 
     // Get key "hello"
