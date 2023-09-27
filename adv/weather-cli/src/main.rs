88d3bd824a3a8cdf3b4c8adbba096a56fec625e8
codeTIT4N <lokeshkumar636@yahoo.com>
2023-09-28 00:45:46 +0530

    weather-cli validation on lat lon range

--- adv/weather-cli/src/main.rs
+++ adv/weather-cli/src/main.rs
@@ -1,19 +1,54 @@
+#![allow(dead_code)]
 use clap::Parser;
 use dotenv;
 use reqwest;
 use serde::Deserialize;
-use std::env;
+use std::{env, ops::RangeInclusive};
+
+const VALID_LAT_RANGE: RangeInclusive<f32> = -90.0..=90.0;
+const VALID_LON_RANGE: RangeInclusive<f32> = -180.0..=180.0;
 
 #[derive(Parser, Debug)]
 #[command(name = "weather")]
 #[command(about = "Weather in your terminal", long_about = None)]
 struct Args {
-    #[clap(long)]
+    #[arg(long, allow_hyphen_values = true, value_parser = parse_lat)]
     lat: f32,
-    #[clap(long)]
+    #[arg(long, allow_hyphen_values = true, value_parser = parse_lon)]
     lon: f32,
 }
 
+fn parse_lat(s: &str) -> Result<f32, String> {
+    let lat: f32 = s
+        .parse()
+        .map_err(|_| format!("`{s}` isn't a valid latitude"))?;
+
+    if VALID_LAT_RANGE.contains(&lat) {
+        Ok(lat as f32)
+    } else {
+        Err(format!(
+            "latitude should be between {}° and {}°",
+            VALID_LAT_RANGE.start(),
+            VALID_LAT_RANGE.end()
+        ))
+    }
+}
+
+fn parse_lon(s: &str) -> Result<f32, String> {
+    let lon: f32 = s
+        .parse()
+        .map_err(|_| format!("`{s}` isn't a valid longitude"))?;
+
+    if VALID_LON_RANGE.contains(&lon) {
+        Ok(lon as f32)
+    } else {
+        Err(format!(
+            "longitude should be between {}° and {}°",
+            VALID_LON_RANGE.start(),
+            VALID_LON_RANGE.end()
+        ))
+    }
+}
 #[derive(Deserialize, Debug)]
 struct Weather {
     description: String,
