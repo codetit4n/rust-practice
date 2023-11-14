f7f33a29003b8b3a2202e4f0d75dd5dfb823f945
Lokesh Kumar <lokeshkumar636@yahoo.com>
2023-11-15 01:19:02 +0530

    axum - error handling, mw

--- adv/axum-example/src/error.rs
+++ adv/axum-example/src/error.rs
@@ -2,13 +2,14 @@ pub type Result<T> = core::result::Result<T, Error>;
 use axum::http::StatusCode;
 use axum::response::{IntoResponse, Response};
 
-#[derive(Debug)]
+#[derive(Debug, Clone, strum_macros::AsRefStr)]
 pub enum Error {
     LoginFail,
 
     // auth errors
     AuthFailNoAuthTokenCookie,
     AuthFailTokenWrongFormat,
+    AuthFaileCtxNotInRequestExt,
     // model errors
     TicketDeleteFaileIdNotFound { id: u64 },
 }
@@ -18,7 +19,13 @@ impl IntoResponse for Error {
     fn into_response(self) -> Response {
         println!("->> {:<12} - {self:?}", "INTO_RES");
 
-        (StatusCode::INTERNAL_SERVER_ERROR, "UNHANDLED_CLIENT_ERROR").into_response()
+        // Create a placeholder Axum response
+        let mut response = StatusCode::INTERNAL_SERVER_ERROR.into_response();
+
+        // Insert the Error into the response
+        response.extensions_mut().insert(self);
+
+        response
     }
 }
 
@@ -29,3 +36,38 @@ impl std::fmt::Display for Error {
 }
 
 impl std::error::Error for Error {}
+
+impl Error {
+    pub fn client_status_and_error(&self) -> (StatusCode, ClientError) {
+        #[allow(unreachable_patterns)]
+        match self {
+            // login
+            Self::LoginFail => (StatusCode::FORBIDDEN, ClientError::LOGIN_FAIL),
+
+            // auth
+            Self::AuthFailNoAuthTokenCookie
+            | Self::AuthFailTokenWrongFormat
+            | Self::AuthFaileCtxNotInRequestExt => (StatusCode::FORBIDDEN, ClientError::NO_AUTH),
+
+            // model
+            Self::TicketDeleteFaileIdNotFound { .. } => {
+                (StatusCode::BAD_REQUEST, ClientError::INVALID_PARAMS)
+            }
+
+            // fallback
+            _ => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                ClientError::SERVICE_ERROR,
+            ),
+        }
+    }
+}
+
+#[derive(Debug, strum_macros::AsRefStr)]
+#[allow(non_camel_case_types)]
+pub enum ClientError {
+    LOGIN_FAIL,
+    NO_AUTH,
+    INVALID_PARAMS,
+    SERVICE_ERROR,
+}
--- adv/axum-example/src/main.rs
+++ adv/axum-example/src/main.rs
@@ -8,6 +8,7 @@ use axum::response::{Html, IntoResponse, Response};
 use axum::routing::{get, get_service};
 use axum::{middleware, Router};
 use serde::{Deserialize, Serialize};
+use serde_json::json;
 use std::net::SocketAddr;
 use tower_cookies::CookieManagerLayer;
 use tower_http::services::ServeDir;
@@ -29,6 +30,10 @@ async fn main() -> Result<()> {
         .merge(web::routes_login::routes())
         .nest("/api", routes_apis)
         .layer(middleware::map_response(main_response_mapper))
+        .layer(middleware::from_fn_with_state(
+            mc.clone(),
+            web::mw_auth::mw_ctx_resolver,
+        ))
         .layer(CookieManagerLayer::new())
         .fallback_service(routes_static());
 
@@ -44,6 +49,28 @@ async fn main() -> Result<()> {
 
 async fn main_response_mapper(res: Response) -> Response {
     println!("->> {:<12} - main_response_mapper", "RES_MAPPER");
+
+    let uuid = uuid::Uuid::new_v4();
+
+    // Get the eventual response error
+    let service_error = res.extensions().get::<Error>();
+    let client_status_error = service_error.map(|e| e.client_status_and_error());
+
+    // If client error, build the new response
+    let error_response = client_status_error.as_ref().map(|(status, client_error)| {
+        let client_error_body = json!({
+        "error":{
+            "type": client_error.as_ref(),
+            "req_uuid": uuid.to_string(),
+            }
+        });
+
+        println!(" -->> client_error_body: {:?}", client_error_body);
+
+        // Build the new response from client_error_body
+        todo!()
+    });
+
     println!();
     res
 }
