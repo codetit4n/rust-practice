7f082b7e90430d82bc74779f3060b210dd80e8ea
Lokesh Kumar <lokeshkumar636@yahoo.com>
2023-12-17 23:54:41 +0530

    removed unnecessory code

--- adv/ai-chatbot/README.md
+++
@@ -1,6 +0,0 @@
-# ai-chatbot
-
-> **Warning**
-> Incomplete!
-
-Referring to: https://www.youtube.com/watch?v=vAjle3c9Xqc
--- adv/ai-chatbot/src/api.rs
+++
@@ -1,5 +0,0 @@
-use leptos::*;
-
-#[server(Converse "/api")]
-pub async fn converse(prompt: Conversation) -> Result<String,ServerFnError>{
-}
