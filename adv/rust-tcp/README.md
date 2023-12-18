16a8e119b7356d10f0e7796bbe2186361ce7a20f
Lokesh Kumar <lokeshkumar636@yahoo.com>
2023-12-19 01:57:43 +0530

    rust tcp parsing

--- adv/rust-tcp/README.md
+++ adv/rust-tcp/README.md
@@ -1,3 +1,7 @@
 # TCP in Rust
 
-Learning by doing. Reffering: https://github.com/Ghvstcode/Rust-Tcp
+> [!WARNING]\
+> Work In Progress!
+
+- https://www.youtube.com/playlist?list=PLqbS7AVVErFivDY3iKAQk3_VAm8SXwt1X
+- https://github.com/Ghvstcode/Rust-Tcp
--- adv/rust-tcp/src/main.rs
+++ adv/rust-tcp/src/main.rs
@@ -1,6 +1,17 @@
-use std::io;
+use std::net::Ipv4Addr;
+use std::{collections::HashMap, io};
+
+mod tcp;
+
+#[derive(Debug, Clone, Copy, Hash, PartialEq, Eq)]
+struct Quad {
+    src: (Ipv4Addr, u16),
+    dst: (Ipv4Addr, u16),
+}
 
 fn main() -> io::Result<()> {
+    let connections: HashMap<Quad, tcp::State> = Default::default();
+
     // Create a new TUN interface in TUN mode
     let nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun)?;
 
@@ -13,10 +24,45 @@ fn main() -> io::Result<()> {
         // receive data from TUN interface and store the number of bytes received in
         // nbytes
         let nbytes = nic.recv(&mut buf[..])?;
+        let _eth_flags = u16::from_be_bytes([buf[0], buf[1]]);
+        let eth_proto = u16::from_be_bytes([buf[2], buf[3]]); // https://en.wikipedia.org/wiki/EtherType#Values
 
-        eprintln!("read {} bytes: {:x?}", nbytes, &buf[..nbytes]);
+        if eth_proto != 0x0800 {
+            // ignore everything other than IPv4 packets
+            continue;
+        }
+
+        match etherparse::Ipv4HeaderSlice::from_slice(&buf[4..nbytes]) {
+            Ok(p) => {
+                let src = p.source_addr();
+                let dst = p.destination_addr();
+                let proto = p.protocol(); // ip level protocol https://www.iana.org/assignments/protocol-numbers/protocol-numbers.xhtml
+
+                if proto != 0x06 {
+                    //not tcp
+                    continue;
+                }
+
+                match etherparse::TcpHeaderSlice::from_slice(&buf[4 + p.slice().len()..]) {
+                    Ok(p) => {
+                        //todo!
+                        eprintln!(
+                            "{} -> {} {}b of tcp to port {}",
+                            src,
+                            dst,
+                            p.slice().len(),
+                            p.destination_port()
+                        )
+                    }
+
+                    Err(e) => {
+                        eprintln!("ignoring weird packet {:?}", e);
+                    }
+                }
+            }
+            Err(e) => {
+                eprintln!("ignoring weird packet {:?}", e);
+            }
+        }
     }
-
-    #[allow(unreachable_code)]
-    Ok(())
 }
