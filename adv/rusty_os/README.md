9560fbc94d8c57d5215ff778269b2ae1a0159ee7
Lokesh Kumar <lokeshkumar636@yahoo.com>
2024-03-20 02:28:27 +0530

    rusty_os kernel init

--- adv/rusty_os/README.md
+++ adv/rusty_os/README.md
@@ -1,3 +1,9 @@
 # Rust based Operating System
 
 Following blog series: https://os.phil-opp.com/
+
+#### Run and pass flag for linker:
+
+```bash
+cargo rustc -- -C link-arg=-nostartfiles
+```
--- adv/rusty_os/src/main.rs
+++ adv/rusty_os/src/main.rs
@@ -1,10 +1,18 @@
-#![no_std]
+#![no_std] // disable standard library
+#![no_main] // disable all Rust-level entry points
 
 use core::panic::PanicInfo;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    // custom panic handler
     loop {}
 }
 
-//continue from https://os.phil-opp.com/freestanding-rust-binary/#disabling-unwinding
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    // custom entry point
+    loop {}
+}
+
+// ctd from https://os.phil-opp.com/minimal-rust-kernel/#target-specification
