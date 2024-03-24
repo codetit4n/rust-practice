781f02ddc86aba03ce30e16631bf659923d687ea
Lokesh Kumar <lokeshkumar636@yahoo.com>
2024-03-24 16:08:57 +0530

    os - vga buffer

--- adv/rusty_os/src/main.rs
+++ adv/rusty_os/src/main.rs
@@ -2,6 +2,7 @@
 #![no_main] // disable all Rust-level entry points
 
 use core::panic::PanicInfo;
+mod vga_buffer;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
@@ -14,13 +15,15 @@ static HELLO: &[u8] = b"Namaste World!";
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     // custom entry point
-    let vga_buffer = 0xb8000 as *mut u8;
+    //let vga_buffer = 0xb8000 as *mut u8;
 
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0x6;
-        }
-    }
+    //for (i, &byte) in HELLO.iter().enumerate() {
+    //    unsafe {
+    //        *vga_buffer.offset(i as isize * 2) = byte;
+    //        *vga_buffer.offset(i as isize * 2 + 1) = 0x6;
+    //    }
+    //}
+
+    vga_buffer::print_something();
     loop {}
 }
---
+++ adv/rusty_os/src/vga_buffer.rs
@@ -0,0 +1,118 @@
+use core::fmt;
+use volatile::Volatile;
+
+#[allow(dead_code)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u8)] // repr(u8) tells the compiler to represent the enum as an u8
+pub enum Color {
+    Black = 0,
+    Blue = 1,
+    Green = 2,
+    Cyan = 3,
+    Red = 4,
+    Magenta = 5,
+    Brown = 6,
+    LightGray = 7,
+    DarkGray = 8,
+    LightBlue = 9,
+    LightGreen = 10,
+    LightCyan = 11,
+    LightRed = 12,
+    Pink = 13,
+    Yellow = 14,
+    White = 15,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(transparent)] // repr(transparent) tells the compiler to ensure that the
+                     // struct has the exact same data layout as u8
+struct ColorCode(u8);
+
+impl ColorCode {
+    fn new(foreground: Color, background: Color) -> ColorCode {
+        ColorCode((background as u8) << 4 | (foreground as u8))
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(C)] // repr(C) tells the compiler to represent the struct as if it were a C struct
+struct ScreenChar {
+    ascii_character: u8,
+    color_code: ColorCode,
+}
+
+const BUFFER_HEIGHT: usize = 25;
+const BUFFER_WIDTH: usize = 80;
+
+#[repr(transparent)] // to ensure it has the same memory layout as its single field
+struct Buffer {
+    chars: [[Volatile<ScreenChar>; BUFFER_WIDTH]; BUFFER_HEIGHT],
+}
+
+// writng to screen
+pub struct Writer {
+    column_position: usize,
+    color_code: ColorCode,
+    buffer: &'static mut Buffer,
+}
+
+impl Writer {
+    pub fn write_byte(&mut self, byte: u8) {
+        match byte {
+            b'\n' => self.new_line(),
+            byte => {
+                if self.column_position >= BUFFER_WIDTH {
+                    self.new_line();
+                }
+
+                let row = BUFFER_HEIGHT - 1;
+                let col = self.column_position;
+
+                let color_code = self.color_code;
+                self.buffer.chars[row][col].write(ScreenChar {
+                    ascii_character: byte,
+                    color_code,
+                });
+                self.column_position += 1;
+            }
+        }
+    }
+
+    pub fn write_string(&mut self, s: &str) {
+        for byte in s.bytes() {
+            match byte {
+                // printable ASCII byte or newline
+                0x20..=0x7e | b'\n' => self.write_byte(byte), // anything between space and ~
+                // not part of printable ASCII range
+                _ => self.write_byte(0xfe), // print ■
+            }
+        }
+    }
+
+    fn new_line(&mut self) {
+        //todo
+    }
+}
+
+// write! macro support
+impl fmt::Write for Writer {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        self.write_string(s);
+        Ok(())
+    }
+}
+
+pub fn print_something() {
+    use core::fmt::Write;
+    let mut writer = Writer {
+        column_position: 0,
+        color_code: ColorCode::new(Color::Yellow, Color::Black),
+        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
+    };
+
+    writer.write_byte(b'N');
+    writer.write_string("amaste! ");
+    write!(writer, "The numbers are {} and {}", 42, 1.0 / 3.0).unwrap();
+}
+
+//continue from https://os.phil-opp.com/vga-text-mode/#newlines
